e6ac96c4831654e93aa3f7df167c1670e933014a
denzel fuentes <denzel.fuentes.n@gmail.com>
2023-10-09 04:30:30 -0400

    user update

--- server/modules/users/user.routes.js
+++ server/modules/users/user.routes.js
@@ -7,9 +7,10 @@ import UserController from "./controllers/user.controller.js";
 
 const CtrlUser = new UserController(UserRecord);
 
-router.post('/create',authJwt.verify
-                            ,type_user.verify_super_admin
-                            ,CtrlUser.create_record);
+// router.post('/create',authJwt.verify
+//                             ,type_user.verify_super_admin
+//                             ,CtrlUser.create_record);
+router.post('/create',CtrlUser.create_record);
 router.delete("/:id",CtrlUser.delete_record);
 router.put("/:id",CtrlUser.update_record);
 router.get('/all',authJwt.verify,CtrlUser.get_all_records);
